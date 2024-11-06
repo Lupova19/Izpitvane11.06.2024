d2aeb97035101d30e64fc6c3a0b57bc2d77183c1
STEM-1-11 <STEM-1-11@DESKTOP-AF3Q9T0>
2024-11-06 11:42:14 +0200

    Add project files.

---
+++ PassportsZadacha/Passport.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassportsZadacha
+{
+    public class Passport
+    {
+        private string name;
+        private int egn;
+        private string number;
+        private string date;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Egn
+        {
+            get { return egn; }
+            set 
+            {
+                if (egn > 10 && egn < 10)
+                {
+                    throw new ArgumentException("Vavedeni sa po-malko ili poveche cifri za EGN");
+                } 
+                this.egn = value;
+            }
+        }
+        public string Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+        public string Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+        public Passport(string name, int egn, string number, string date) 
+        {
+            this.Name = name;
+            this.Egn = egn;
+            this.Number = number;
+            this.Date = date;
+        }
+        public Passport() 
+        {
+            this.Name = "Ivan";
+            this.Egn = 0749555123;
+            this.Number = "ABM43";
+            this.Date = "10.02.2024g.";
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Name:{this.Name}, EGN:{this.Egn}, Number of passport:{this.Number}, Date:{this.Date}");
+        }
+        
+    }
+}
---
+++ PassportsZadacha/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassportsZadacha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Passport passport = new Passport();
+                passport.Print();
+                List<Passport> passports = new List<Passport>();
+                int n = int.Parse(Console.ReadLine());
+                for (int i = 0; i < n; i++)
+                {
+                    var danni = Console.ReadLine().Split();
+                    Passport passportsDanni = new Passport(danni[0], int.Parse(danni[1]), danni[2], danni[3]);
+                    passports.Add(passportsDanni);
+                }
+                passports.ForEach(x => x.Print());
+                Console.Write("Vavedi ime:");
+                string name = Console.ReadLine();
+                passports.Where(x => x.Name == name).ToList().ForEach(x => x.Print());
+                var youngestPerson = passports.Min(x => x.Egn);
+                Console.WriteLine($"Min stojnost: {youngestPerson}");
+
+                Console.WriteLine("Sortirani po data v nixhodqsht red:");
+                passports.OrderByDescending(x => x.Date).ToList().ForEach(x => x.Print());
+
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+
+
+        }
+    }
+}
